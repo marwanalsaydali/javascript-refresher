8f74e28692dcdf1fd9f5fbe692b9441b372f5f5e
marwanalsaydali <hey@marwanalsaydali.com>
2021-01-02 14:37:06 -0500

    The basic refresh

---
+++ index.js
@@ -0,0 +1,41 @@
+// let name : "Marwam";
+// let lastName: "Alsaydali";
+// variable names should not start with number
+// variable names should not have a space
+// I use canel case in my naming 
+// I use meanable names 
+
+// const name : "Marwan";
+// let title : "Web Developer";
+// I use const wben I know the varianle wont change
+
+//  Primitive Types
+// String
+// Number
+// boolean
+// undefine
+// null I usually use it to clear a value of the variable 
+
+// typeof  use to know what is the type of variable
+
+// Reference type
+// Object
+// Arrey 
+// Function
+
+// let person = {
+//  name: "Marwan",
+//  age : 38,
+//  }
+
+// to change the name person.name = "M"; // dot notation
+// or let select = "name" ;
+// person{select} = "M"; //bracket notation
+
+// let list = ["car", 3, null, "4 wheels"];
+
+// function sayHi(name){
+   // console.log('hello' + name);
+// };
+// sayHi(); 
+
